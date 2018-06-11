d08e74fe6821ea09291a4cdb293c71fbe299d390
jbalcita <jbalcita@cj.com>
2018-06-11 11:49:49 -0700

    convert string-keyed maps to Episode structs

--- lib/episode.ex
+++ lib/episode.ex
@@ -1,3 +1,14 @@
 defmodule Episode do
   defstruct [:season, :season_episode_number, :title, :writer, :original_air_date]
+
+  @spec from_string_keyed_map(map()) :: %Episode{}
+  def from_string_keyed_map(map) do
+    %Episode{
+      season: map["season"],
+      season_episode_number: map["season_episode_number"],
+      title: map["title"],
+      writer: map["writer"],
+      original_air_date: map["original_air_date"]
+    }
+  end
 end
--- test/episode_test.exs
+++ test/episode_test.exs
@@ -2,6 +2,7 @@ defmodule EpisodeTest do
   use ExUnit.Case
   doctest Episode
 
+
   test "can instantiate Episode struct with all fields" do
     # given
     plain_map = %{
@@ -23,4 +24,27 @@ defmodule EpisodeTest do
     assert episode.writer == "John Carlo Balcita"
     assert episode.original_air_date == "2018-06-09"
   end
+
+  test "can convert string-keyed map to struct" do
+    # given
+    string_keyed_map = %{
+      "season" => 9,
+      "season_episode_number" => 11,
+      "title" => "JC Goes to Camp",
+      "writer" => "Kobe Bryant",
+      "original_air_date" => "2018-06-11"
+    }
+
+    # when
+    episode = Episode.from_string_keyed_map(string_keyed_map)
+
+    # then
+    assert Map.has_key?(episode, :__struct__)
+    assert episode.season == 9
+    assert episode.season_episode_number == 11
+    assert episode.title == "JC Goes to Camp"
+    assert episode.writer == "Kobe Bryant"
+    assert episode.original_air_date == "2018-06-11"
+  end
+
 end
